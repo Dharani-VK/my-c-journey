7f657e294f778adfa278e2111310dea1a575c664
Dharani V K <dharani.vk05@gmail.com>
2025-06-15 19:25:30 +0530

    Create PrintMonth.c

---
+++ conditional_looping/PrintMonth.c
@@ -0,0 +1,57 @@
+//9)Write a program to print the respective month name based on given input
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  int a; 
+    scanf("%d" , &a);
+    
+    switch(a){
+        case 1:
+            printf("January");
+            break;
+        case 2:
+            printf("February");
+            break;
+        case 3:
+            printf("March");
+            break;
+        case 4:
+            printf("April");
+            break;
+        case 5:
+            printf("May");
+            break;
+        case 6:
+            printf("June");
+            break;
+        case 7:
+            printf("July");
+            break;
+        case 8:
+            printf("August");
+            break;
+        case 9:
+            printf("September");
+            break;
+        case 10:
+            printf("October");
+            break;
+        case 11:
+            printf("November");
+            break;
+        case 12:
+            printf("December");
+            break;
+        default:
+            printf("Invalid");
+            
+            
+    }
+    return 0;
+}
+
