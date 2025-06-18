e001783ae4a3623779b6126232fadd21bace6b43
Dharani V K <dharani.vk05@gmail.com>
2025-06-18 21:06:24 +0530

    Create 1.c

---
+++ looping/1.c
@@ -0,0 +1,31 @@
+/*Print all the numbers from 1 to n.
+
+Input Format
+
+Integer
+
+Constraints
+
+NIl
+
+Output Format
+
+Set of integers
+
+Sample Input 0
+
+10
+Sample Output 0
+
+1 2 3 4 5 6 7 8 9 10 */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
