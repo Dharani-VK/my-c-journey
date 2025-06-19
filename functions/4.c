079af5b801ede5af012df9210a1a8ac426bf864b
Dharani V K <dharani.vk05@gmail.com>
2025-06-19 19:11:05 +0530

    Create 4.c

---
+++ functions/4.c
@@ -0,0 +1,30 @@
+/*Write a program to print %.
+  nput Format
+
+none
+
+Constraints
+
+none
+
+Output Format
+
+none
+
+Sample Output 0
+
+%%
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    printf("%%");
+    printf("%%");
+    return 0;
+}
